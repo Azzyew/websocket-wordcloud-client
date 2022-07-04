81d7827136b9414f467362757837f37d1c7af582
Azzyew <laisadnevescosta@gmail.com>
2022-07-04 09:46:10 -0300

    feat/adding styles to project

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
 import ReactWordcloud from "react-wordcloud";
+
+import { Input, Button, Box, Center, Heading } from "@chakra-ui/react";
+
 import { socket } from "../../services/socket";
 
 export const Home = () => {
   const [text, setText] = useState("");
   const [words, setWords] = useState<any[]>([]);
-
-  const isValidText = text.length > 0 && text.length < 30;
 
   useEffect(() => {
     const receivedMessage = (message: string) => {
@@ -20,42 +21,73 @@ export const Home = () => {
     });
   }, [words, text]);
 
+  const valueArr = words.map((w) => {
+    return w.text;
+  });
+  const isDuplicate = valueArr.some((w, idx) => {
+    return valueArr.indexOf(w) !== idx;
+  });
+
+  const isValidText = text.length > 0 && text.length < 30 && !isDuplicate;
+
   const sendMessage = () => {
     if (isValidText) {
-      socket.emit("message", text);
+      const formattedData = { text: text, value: 1 };
+      socket.emit("message", formattedData);
       setText("");
     }
   };
 
   return (
     <>
-      <div>
-        <input
-          type="text"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          placeholder="Enter text..."
-        />
-        <button type="button" onClick={() => sendMessage()}>
-          Send
-        </button>
-        <div style={{ height: 400, width: 600 }}>
-          <ReactWordcloud
-            words={words}
-            options={{
-              rotations: 0,
-              fontSizes: [15, 50],
-              fontWeight: "bold",
-              fontFamily: "Nunito",
-              deterministic: true,
-              randomSeed: "websocket",
-              scale: "linear",
-              enableTooltip: false,
-            }}
-            maxWords={100}
+      <Box h="100vh" bg="#EDF2F7">
+        <Box bg="#0B0B45" h="70px" w="100%">
+          <Center>
+            <Heading fontSize="30px" mt={3}>
+              Websocket Wordcloud
+            </Heading>
+          </Center>
+        </Box>
+        <Center mt={10}>
+          <Input
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Enter text"
+            _placeholder={{ opacity: 1, color: "#0B0B45" }}
+            color="#0B0B45"
+            width="50vw"
+            borderColor="#0B0B45"
+            focusBorderColor="#0B0B45"
           />
-        </div>
-      </div>
+          <Button
+            type="button"
+            bg="#0B0B45"
+            ml={1}
+            onClick={() => sendMessage()}
+          >
+            Send
+          </Button>
+        </Center>
+        <Box p={60} mr={100}>
+          <Center>
+            <ReactWordcloud
+              words={words}
+              options={{
+                rotations: 0,
+                fontSizes: [15, 50],
+                fontWeight: "bold",
+                fontFamily: "Nunito",
+                deterministic: true,
+                randomSeed: "websocket",
+                scale: "linear",
+                enableTooltip: false,
+              }}
+              maxWords={100}
+            />
+          </Center>
+        </Box>
+      </Box>
     </>
   );
 };
